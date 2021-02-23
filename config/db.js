38d89f8a9e0455c1136a7c102ee6c2928df805b1
outcasted9044 <realweirdo904@gmail.com>
2021-02-23 17:42:33 +0300

    layout changes

--- config/db.js
+++ config/db.js
@@ -1,27 +1,8 @@
 const mongoose = require('mongoose');
 
-// const createConnectionLocal = async () => {
-//   try {
-//     await mongoose.connect(process.env.DATABASE_LOCAL, {
-//       useNewUrlParser: true,
-//       useFindAndModify: false,
-//       useCreateIndex: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('mongodb connected...');
-//   } catch (error) {
-//     console.error(`error: ${error.message}`);
-//   }
-// };
-
-const createConnectionAtlas = async () => {
-  const DB = process.env.DATABASE_ATLAS.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-  );
-
+const createConnectionLocal = async () => {
   try {
-    await mongoose.connect(DB, {
+    await mongoose.connect(process.env.DATABASE_LOCAL, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true,
@@ -33,5 +14,24 @@ const createConnectionAtlas = async () => {
   }
 };
 
-module.exports = createConnectionAtlas;
-// module.exports = createConnectionLocal;
+// const createConnectionAtlas = async () => {
+//   const DB = process.env.DATABASE_ATLAS.replace(
+//     '<PASSWORD>',
+//     process.env.DATABASE_PASSWORD
+//   );
+
+//   try {
+//     await mongoose.connect(DB, {
+//       useNewUrlParser: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('mongodb connected...');
+//   } catch (error) {
+//     console.error(`error: ${error.message}`);
+//   }
+// };
+
+// module.exports = createConnectionAtlas;
+module.exports = createConnectionLocal;
